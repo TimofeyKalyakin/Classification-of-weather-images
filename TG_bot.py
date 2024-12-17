8a02068128b5da492feb44d6e0a9bd0c9d0dfefd
TimofeyKalyakin <t.s.kalyakin@gmail.com>
2024-12-17 22:34:09 +0300

    TG_bot commit

---
+++ TG_bot.py
@@ -0,0 +1,79 @@
+import io
+import logging
+import os
+import torch
+
+from dotenv import load_dotenv
+from PIL import Image
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
+from torchvision import transforms
+from timm import create_model
+
+# Настройки логирования
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+# Маппинг индексов классов на тип погоды
+IDX_TO_CLASS = {
+    0: 'dew', 1: 'fog/smog', 2: 'frost', 3: 'glaze', 4: 'hail',
+    5: 'lightning', 6: 'rain', 7: 'rainbow', 8: 'rime',
+    9: 'sandstorm', 10: 'snow'
+}
+
+# Загрузка модели
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = create_model('vgg16', pretrained=False, num_classes=11)
+model.load_state_dict(torch.load('vgg16_weather_classifier.pth'))
+model = model.to(device)
+model.eval()
+
+# Токен бота
+load_dotenv()
+TOKEN = os.getenv('TELEGRAM_TOKEN')
+
+
+# Функция для классификации изображения
+def classify_image(model, image):
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+    image = transform(image).unsqueeze(0).to(device)
+
+    with torch.no_grad():
+        predictions = model(image)
+
+    prediction_probs = torch.nn.functional.softmax(predictions, dim=1)
+    predicted_class = torch.argmax(prediction_probs, dim=1).item()
+
+    return IDX_TO_CLASS[predicted_class], prediction_probs[0, predicted_class].item() * 100
+
+
+# Обработчик команды /start
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        'Привет! Отправь мне изображение погоды, и я попробую её определить.'
+    )
+
+
+# Обработчик изображений
+async def handle_image(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    file = await update.message.photo[-1].get_file()
+    image_bytes = await file.download_as_bytearray()
+    image = Image.open(io.BytesIO(image_bytes))
+
+    # Классификация изображения
+    class_name, probability = classify_image(model, image)
+    await update.message.reply_text(f'С вероятностью {probability:.2f}% это {class_name}.')
+
+
+# Запуск бота
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(TOKEN).build()
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(MessageHandler(filters.PHOTO, handle_image))
+    application.run_polling()
